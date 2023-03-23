1d5aa5e64d6cbe532fdfca3278286475087e4179
A.Emre ELP <a.emreelp@gmail.com>
2023-03-23 04:18:47 +0300

    IgetTheDataAboutWeather
    We can change STNM OR MONTH information to get other mounth values

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import requests
 import bs4
-import pandas as pd
+import requests
+import csv
 
-url = "https://weather.uwyo.edu/cgi-bin/sounding?region=naconf&TYPE=TEXT%3ALIST&YEAR=2013&MONTH=02&FROM=0100&TO=2812&STNM=17030"
-result = requests.get(url,verify=False)
+for year in range(13,23):
+    if(year%4==0):
+        day=29
+    else:
+        day=28
+    url = "https://weather.uwyo.edu/cgi-bin/sounding?region=naconf&TYPE=TEXT%3ALIST&YEAR=20"+str(year)+"&MONTH=02&FROM=0100&TO="+str(day)+"12&STNM=17030"
+    result = requests.get(url,verify=False)
 
-soup = bs4.BeautifulSoup(result.text,'lxml')
-cases = []
-cases = soup.find_all('pre')
-for i in range(0,len(cases)):
-    if(i%2==1):
-        print(cases[i])
+    soup = bs4.BeautifulSoup(result.text,'lxml')
+    cases = []
+    cases = soup.find_all('pre')
+
+    file = open('subatLast.csv','a')
+    file = csv.writer(file)
+
+    for i in range(0,len(cases)):
+        if(i%2==1):
+            k = cases[i].text.replace('\n','""')
+            print(k)
+            file.writerow([k])
+
+
+
+
+
 
